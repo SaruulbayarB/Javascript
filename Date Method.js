d53d38f48c7f0b1a49f3c95572251b6f473bc9ec
SaruulbayarB <saruuleagle@gmail.com>
2025-08-01 12:29:49 +0800

    date

---
+++ Date Method.js
@@ -0,0 +1,17 @@
+const Date = new Date();
+const year = Date.getFullYear();
+const month = Date.getMonth();
+const day = Date.getDay();
+
+const hour = Date.getHours();
+
+console.log(year);
+console.log(month);
+console.log(day);
+console.log(hour);
+
+
+function currentdate = Date; 
+
+const startedDate = new Date("2025-06-09"); 
+
